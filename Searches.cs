8cae17e0b03d349c00bd6847668b3114ed48f430
Anthony Bonner <CyborgBrass@outlook.com>
2020-11-05 14:19:08 -0500

    Added the Search class.

---
+++ Searches.cs
@@ -0,0 +1,38 @@
+// Pseudocode for Room and Item Searches
+namespace Searches
+{
+    public class LivingRoomSearch
+    {
+        /* RoomSearch()
+            MantleSearch()
+            LoveseatSearch() - 'nothing'
+            CouchSearch()
+        */
+    }
+    public class MasterBedroomSearch
+    {
+        /* ClosetLeftSearch()
+            ClosetRightSearch()
+            BedSearch()
+            DresserSearch()
+            Other?
+        */
+    }
+    public class BedroomSearch
+    {
+        /* ClosetOneSearch()
+            ClosetTwoSearch()
+            Bookcase() - 'puzzle'?
+            BedSearch()
+        */    
+    }
+    public class KitchenSearch
+    {
+        /* CabinetSearch()
+            OpenWallSpot
+            RoomSearchK() - floor Dr
+            More
+        */    
+    }
+
+}
